11bdcc08aef5e2927b5c0d9f8b920f66e02ecc7a
liambaker93 <liam_baker@live.com>
2026-01-06 16:16:17 +0000

    initialise django project and set up initial shows app with loaded url page and navbar. Navbar is currently not displaying styled correctly due to html not being linked up with bootstrap, and models have yet to be migrated.

--- combined_actors/settings.py
+++ combined_actors/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -37,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'shows',
 ]
 
 MIDDLEWARE = [
@@ -54,7 +56,9 @@ ROOT_URLCONF = 'combined_actors.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            os.path.join(BASE_DIR, 'templates'),
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -104,7 +108,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'GMT'
 
 USE_I18N = True
 
---
+++ shows/models.py
@@ -0,0 +1,35 @@
+from django.db import models
+from django.core.exceptions import ValidationError
+
+# Create your models here.
+
+
+class Shows(models.Model):
+    """
+    This model is for displaying show information on the homepage and will hold more
+    data than will potentially be shown, but can be used for filtering by users and by admins.
+    """
+    class Meta:
+        verbose_name = 'Show'
+        verbose_name_plural = 'Shows'
+
+    name = models.CharField(max_length=256, blank=False)
+    start_date = models.DateTimeField()
+    end_date = models.DateTimeField()
+    director = models.CharField(max_length=256, blank=False)
+    writer = models.CharField(max_length=256, blank=False)
+    theatre = models.CharField(max_length=256, blank=False)
+    description = models.TextField(blank=True, null=True)
+    on_sale = models.BooleanField(default=False, blank=True)
+    in_future = models.BooleanField(default=False, blank=True)
+
+    def timeCheck(self):
+        """
+        Logic to ensure that the end date is after the start date
+        """
+        if self.start_date and self.end_date:
+            if self.end_date <= self.start_date:
+                raise ValidationError("The end date must be after the start date")
+
+    def __str__(self):
+        return f"{self.name} from {self.start_date} to {self.end_date} at the {self.theatre}"
